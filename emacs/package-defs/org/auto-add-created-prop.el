fa7d975392a3fa7d0af2b9197ae70579c578ee3e
Patrick Charles-Lundaahl <plundaahl@protonmail.com>
2021-05-24 14:04:24 -0700

    [emacs] auto-add IDs to all headings

--- emacs/package-defs/org/auto-add-created-prop.el
+++ emacs/package-defs/org/auto-add-created-prop.el
@@ -7,13 +7,15 @@
     (org-entry-put nil "CREATED" (format-time-string "[%Y-%m-%d %a %H:%M]"))))
 
 (add-hook 'org-insert-heading-hook
-         #'(lambda()
-               (save-excursion
-                    (org-back-to-heading)
-                    (pcl/insert-created-timestamp-if-not-present))))
+          #'(lambda()
+              (save-excursion
+                (org-back-to-heading)
+		(org-id-get (point) t)
+                (pcl/insert-created-timestamp-if-not-present))))
 
 (add-hook 'org-capture-before-finalize-hook
-         #'(lambda()
-               (save-excursion
-                    (org-back-to-heading)
-                    (pcl/insert-created-timestamp-if-not-present))))
+          #'(lambda()
+              (save-excursion
+                (org-back-to-heading)
+		(org-id-get (point) t)
+                (pcl/insert-created-timestamp-if-not-present))))
