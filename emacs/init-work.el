45f9168442cf47e93a5ee76e578e20fab9bde8d7
Patrick Charles-Lundaahl <patrick.lundaahl@elasticpath.com>
2021-05-28 15:43:13 -0700

    [emacs] work setup update

--- emacs/init-work.el
+++ emacs/init-work.el
@@ -14,42 +14,70 @@
   (load (rel-file "package-defs/org/base.el"))
   (load (rel-file "package-defs/org-ql/base.el")))
 
-(setq org-tags-exclude-from-inheritance '("PROJECT" "1_1_Mark" "MEETING"))
+(setq org-tags-exclude-from-inheritance
+      '("PROJECT"
+	"REMINDER"
+	"1_1_Mark"
+	"MEETING"))
 
-(setq org-agenda-files '("~/org"
-			 "~/org/areas"
-			 "~/org/vault"
-			 "~/org/projects"))
+(setq org-agenda-files
+      '("~/org"
+	"~/org/areas"
+	"~/org/vault"
+	"~/org/projects"))
 
-(setq org-tag-persistent-alist '(
-				 ("Index" . ?i)
-				 ("Note" . ?n)
-				 ("Tool" . ?t)
-				 ("Concept" . ?c)
-				 (:startgrouptag)
-				 ("Reference" . ?R)
-				 ("Article" . ?A)
-				 ("Video" . ?V)
-				 (:endgrouptag)
-				 ("Appointment" . ?a)
-				 ("Metric" . ?m)
-				 ))
+(setq org-tag-persistent-alist
+      '(("Index" . ?i)
+	("Note" . ?n)
+	("Tool" . ?t)
+	("Concept" . ?c)
+	(:startgrouptag)
+	("Reference" . ?R)
+	("Article" . ?A)
+	("Video" . ?V)
+	(:endgrouptag)
+	("Appointment" . ?a)
+	("Metric" . ?m)
+	("MEETING" . ?M)
+	("REMINDER" . ?r)
+	))
+
+(setq org-todo-keywords
+      '((sequence
+	 ;; Standard Tasks
+	 "BLOCKED(b)"
+	 "TODO(t)"
+	 "NEXT(n)"
+	 "|"
+	 "DONE(d)"
+	 "CANCELLED(c)"
+	 )
+	(sequence
+	 ;; Meetings
+	 "PENDING(p)"
+	 "|"
+	 "ATTENDED(a)"
+	 "SKIPPED(s)"
+	 )))
 
 (setq org-agenda-custom-commands
-   '(("d" "Daily Action View"
-      ((agenda ""
-	       ((org-agenda-overriding-header "Agenda")
-		(org-agenda-span 1)
-		(org-agenda-skip-scheduled-if-done t)
-		(org-agenda-skip-deadline-if-done t)
-		(org-agenda-hide-tags-regexp "\\|*")))
-       (tags "+SCHEDULED>\"<-1d>\"+SCHEDULED<\"<+1d>\"+REMINDER-TODO=\"DONE\"-TODO=\"CANCELLED\""
-	     ((org-agenda-overriding-header "Reminders & Events")
-	      (org-agenda-prefix-format "- ")
-	      (org-agenda-hide-tags-regexp "\\|*")))
-       (tags "+CLOSED>\"<-1d>\""
-	     ((org-agenda-overriding-header "Complete")))))
-))
+      '(("d" "Daily Action View"
+	 ((tags "+SCHEDULED>\"<-1d>\"+SCHEDULED<\"<+1d>\"+REMINDER-TODO=\"DONE\"-TODO=\"CANCELLED\""
+		((org-agenda-overriding-header "Reminders & Events")
+		 (org-agenda-prefix-format "- ")
+		 (org-agenda-hide-tags-regexp "\\|*")))
+	  (agenda ""
+		  ((org-agenda-overriding-header "Agenda")
+		   (org-agenda-tag-filter-preset
+		    '("-REMINDER"))
+		   (org-agenda-span 1)
+		   (org-agenda-span 1)
+		   (org-agenda-skip-scheduled-if-done t)
+		   (org-agenda-skip-deadline-if-done t)
+		   (org-agenda-hide-tags-regexp "\\|*")))
+	  (tags "+CLOSED>\"<-1d>\""
+		((org-agenda-overriding-header "Complete")))))
+	))
 
 (setq org-capture-templates '(
      ("t" "Todo" entry
@@ -66,7 +94,9 @@ SCHEDULED: %t"
        :empty-lines-after 1
        :time-prompt t)
 
-     ("w" "Weekly Review" entry
+     ("r" "Review...")
+     
+     ("rw" "Weekly Review" entry
       (file+headline "~/org/reviews.org" "Weekly")
       (file "~/dotfiles/emacs/capture-templates/work/review-weekly.org")
       :empty-lines-after 1
@@ -75,7 +105,7 @@ SCHEDULED: %t"
 
      ("m" "Meeting" entry
       (file+datetree "~/org/!Tasks.org")
-       "* TODO Meeting: %? %u :MEETING:
+       "* PENDING %? %u :MEETING:
 SCHEDULED: %T"
        :tree-type week
        :empty-lines 1
