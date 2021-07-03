72192e538038ca72d29ab83b4d5c5f8b0f18f836
Patrick Charles-Lundaahl <plundaahl@protonmail.com>
2021-07-03 09:52:12 -0700

    [emacs] update pcl/orgq to also handle element data structures

--- emacs/func/orgq.el
+++ emacs/func/orgq.el
@@ -1,23 +1,24 @@
 ;; -*- mode: elisp -*-
 
 ;; Queries for org headlines
-(defun pcl/orgq (pom query &rest args)
+(defun pcl/orgq (pom-or-element query &rest args)
   "Queries org headlines (runs QUERY at point POM with ARGS). Queries can be
 configured via the variable pcl/orgq-queries, so you can add new ones as you
 feel like it"
   (save-excursion
-    (goto-char (or pom (point)))
-    (org-back-to-heading-or-point-min t)
-    (apply (plist-get pcl/orgq-queries query)
-	   (point)
-	   args)))
+    (let ((element (cond ((listp pom-or-element) pom-or-element)
+			 ('t (goto-char (or pom-or-element (point)))
+			     (org-back-to-heading-or-point-min t)
+			     (org-element-headline-parser (point))))))
+      (apply (plist-get pcl/orgq-queries query)
+	     element
+	     args))))
 
 ;; Set of available queries
 (setq pcl/orgq-queries
       '(
 	:prop
-	(lambda (pom property)
-       	  (let ((element (org-element-headline-parser pom)))
-	    (when (equal (car element) 'headline)
-	      (plist-get (cadr element) property))))
-	))
+	(lambda (element property)
+	  (when (equal (car element) 'headline)
+	    (plist-get (cadr element) property))))
+      )
